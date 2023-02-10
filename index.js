060f1747a5e15532bb1ef7acb320e7aa671ef023
juandogan <juandogan@gmail.com>
2023-02-10 19:33:13 -0300

    fix

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ console.log({'url': url })
 
 app.post('/upload2', multiPartMiddleware, (req,res)=>{   
     var link = req.files['upload'].path      
-    var url = 'http://66.97.44.139:3000/upload/'+ link.slice(8) 
+    var url = 'http://66.97.44.139/upload/'+ link.slice(8) 
     console.log({'url': url })
     res.json({'url':url });
     
